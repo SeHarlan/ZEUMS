f6fd6bc2d5e056ca53bb947a56fc9d814a45eca9
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2025-10-22 21:56:12 +0700

    add close button to sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({
             </UserContextProvider>
           </AuthContextProvider>
         </WalletContextProvider>
-        <Toaster position="top-center" richColors/>
+        <Toaster position="top-center" richColors={true} closeButton={true} />
       </body>
     </html>
   );
